f5e6d200c4ed826e44272a3eeb679e3e6ed967db
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-05-16 14:19:14 +0530

    Update cart.js

--- back-end/src/controller/cart.js
+++ back-end/src/controller/cart.js
@@ -50,7 +50,7 @@ exports.addItemToCart = (req, res) => {
       cart.save((error, cart) => {
         if (error) return res.status(400).json({ error });
         if (cart) {
-          return res.status(201).json({ cart });
+          return res.status(200).json({ cart });
         }
       });
     }
