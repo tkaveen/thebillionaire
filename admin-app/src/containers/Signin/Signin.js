2833c99d0ec15dc4153009e655959fe15c022876
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-05-22 14:18:32 +0530

    Login Request

--- admin-app/src/containers/Signin/Signin.js
+++ admin-app/src/containers/Signin/Signin.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { Container, Form, Row, Col, Button } from "react-bootstrap";
 import Layout from "../../components/Layout";
+import { login } from "../../actions";
+import { useDispatch } from "react-redux";
 
 export default function Signin() {
+  const dispatch = useDispatch();
+
+  const userLogin = (e) => {
+    e.preventDefault();
+
+    const user = {
+      email: "kaveen@gmail.com",
+      password: "123456",
+    };
+
+    dispatch(login(user));
+  };
+
   return (
     <Layout>
       <Container>
         <Row style={{ marginTop: "50px" }}>
           <Col md={{ span: 6, offset: 3 }}>
-            <Form>
+            <Form onSubmit={userLogin}>
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
