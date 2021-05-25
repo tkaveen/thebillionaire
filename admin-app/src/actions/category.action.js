7c1be3ed42f1327a95aa2ad4828dd1a37eb14277
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-05-26 00:18:26 +0530

    category updated

--- admin-app/src/actions/category.action.js
+++ admin-app/src/actions/category.action.js
@@ -1,9 +1,42 @@
 // import axios from "axios";
 import axios from "../helpers/axios";
+import { categoryConstants } from "./constants";
 
 export const getAllCategory = () => {
   return async (dispatch) => {
+    dispatch({ type: categoryConstants.GET_ALL_CATEGORIES_REQUEST });
     const res = await axios.get(`category/getcategory`);
     console.log(res);
+    if (res.status === 200) {
+      const { categories } = res.data;
+
+      dispatch({
+        type: categoryConstants.GET_ALL_CATEGORIES_SUCCESS,
+        payload: { categories: categories },
+      });
+    } else {
+      dispatch({
+        type: categoryConstants.GET_ALL_CATEGORIES_FAILURE,
+        payload: { error: res.data.error },
+      });
+    }
+  };
+};
+
+export const addCategory = (form) => {
+  return async (dispatch) => {
+    dispatch({ type: categoryConstants.ADD_NEW_CATEGORY_REQUEST });
+    const res = await axios.post(`/category/create`, form);
+    if (res.status === 201) {
+      dispatch({
+        type: categoryConstants.ADD_NEW_CATEGORY_SUCCESS,
+        payload: res.data.category,
+      });
+    } else {
+      dispatch({
+        type: categoryConstants.ADD_NEW_CATEGORY_FAILURE,
+        payload: res.data.error,
+      });
+    }
   };
 };
--- admin-app/src/containers/category/index.js
+++ admin-app/src/containers/category/index.js
@@ -1,16 +1,44 @@
-import React, { useEffect } from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Button, Modal } from "react-bootstrap";
 import Layout from "../../components/Layout/index";
 import SaveIcon from "@material-ui/icons/Save";
 import { makeStyles } from "@material-ui/core/styles";
-import { useDispatch } from "react-redux";
-import { getAllCategory } from "../../actions";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCategory, addCategory } from "../../actions";
+import AddCategoryModal from "../../components/Modal/AddCategoryModal";
+import Input from "../../components/Ui/Input";
 
 export default function Category() {
+  const category = useSelector((state) => state.category);
+  const [categoryName, setCategoryName] = useState("");
+  //   const [parentCategoryId, setParentCategoryId] = useState("");
+  //   const [categoryImage, setCategoryImage] = useState("");
+  const [show, setShow] = useState(false);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getAllCategory());
   }, []);
+
+  const handleClose = () => {
+    const form = new FormData();
+    form.append("name", categoryName);
+    dispatch(addCategory(form));
+
+    setShow(false);
+  };
+  const handleShow = () => setShow(true);
+
+  const renderCategories = (categories) => {
+    let myCategories = [];
+    for (let category of categories) {
+      myCategories.push(<li key={category.name}>{category.name}</li>);
+    }
+    return myCategories;
+  };
+
+  //   const handleCategoryImage = (e) => {
+  //     setCategoryImage(e.target.files[0]);
+  //   };
 
   return (
     <Layout sidebar>
@@ -20,13 +48,42 @@ export default function Category() {
             <div style={{ display: "flex", justifyContent: "space-between" }}>
               <h3>Category</h3>
 
-              <button type="button" class="btn btn-dark">
+              <button type="button" class="btn btn-dark" onClick={handleShow}>
                 Add Category
               </button>
             </div>
           </Col>
         </Row>
+        <Row>
+          <Col md={12}>
+            <ul>{renderCategories(category.categories)}
+            
+            </ul>
+          </Col>
+        </Row>
       </Container>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add New Category</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Input
+            value={categoryName}
+            placeholder={`Category Name`}
+            onChange={(e) => setCategoryName(e.target.value)}
+          />
+          {/* <Input
+            type="file"
+            name="categoryImage"
+            onChange={handleCategoryImage}
+          /> */}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Layout>
   );
 }
