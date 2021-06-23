7a3490469fcc5e2129a24ef71143248e3d9c216e
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-06-23 20:19:35 +0530

    Update Footer.js

--- thebillionaire/src/components/Pages/Footer/Footer.js
+++ thebillionaire/src/components/Pages/Footer/Footer.js
@@ -13,7 +13,7 @@ import {
 function Footer() {
   return (
     <div className="footer-container">
-      {/* <section className='footer-subscription'>
+      <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
           Join our exclusive membership to receive the latest news and trends
         </p>
@@ -31,7 +31,7 @@ function Footer() {
             <Button buttonStyle='btn--outline'>Subscribe</Button>
           </form>
         </div>
-      </section> */}
+      </section>
       <div className="footer-links">
         <div className="footer-link-wrapper">
           <div className="footer-link-items">
