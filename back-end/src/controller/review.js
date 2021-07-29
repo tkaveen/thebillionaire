669dd9ccbf031322993d6943e217da6d245ad655
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-07-29 17:20:48 +0530

    Update review.js

--- back-end/src/controller/review.js
+++ back-end/src/controller/review.js
@@ -21,40 +21,17 @@ exports.addReview = async (req, res) => {
 };
 
 exports.getReview = async (req, res) => {
-
   await Review.find({ productId: req.params.id })
-  .populate("userId")
-  .exec()
-  .then((data) => {
-    if (data) {
-      res.status(200).json(data);
-    } else {
-      res.status(404).json({ message: "not found" });
-    }
-  })
-  .catch((err) => {
-    res.status(500).json(err);
-  });
-  // await Review.find({ productId: req.params.id })
-  //   .populate('userId')
-  //   .exec()
-  //   .then((res)=>{
-  //     if(res){
-  //       return res.status(200).json( review );
-  //     }else{
-  //       return res.status(400).json({ err });
-  //     }
-  //   })
-  //   .catch((err) => {
-  //     res.status(500).json(err);
-  //   });
-
-    // .exec((err, review) => {
-    //   if (err) {
-    //     return res.status(400).json({ err });
-    //   }
-    //   if (review) {
-    //     return res.status(200).json( review );
-    //   }
-    // });
+    .populate("userId")
+    .exec()
+    .then((data) => {
+      if (data) {
+        res.status(200).json(data);
+      } else {
+        res.status(404).json({ message: "not found" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 };
